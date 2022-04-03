261c39fac1d57b3e7f6a86e6e89fab4571e1382e
Mnabawy <m_ninja61@yahoo.com>
2022-04-03 14:22:19 +0200

    about component

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,7 +1,34 @@
 import React from "react";
 
+import ME from "../../assets/me-about.jpg";
+import "./about.css";
+
 const About = () => {
-  return <section id="about">About</section>;
+  return (
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="about me" />
+          </div>
+        </div>
+        <div className="about__content">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa
+            quidem, optio ratione dolorem iusto, natus corporis animi aliquid
+            mollitia aperiam voluptatibus exercitationem praesentium nihil fugit
+            porro fuga! Maxime, dolor asperiores!
+          </p>
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
